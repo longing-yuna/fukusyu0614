687d736a243b74391ae8d6b7c878d0a5d72fbf73
pizza_girl <pizza.girl.dat@gmail.com>
2019-06-14 16:11:56 +0900

    今日最後

--- fukusyu0614/Form1.Designer.cs
+++ fukusyu0614/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -43,16 +47,58 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(112, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.Size = new System.Drawing.Size(73, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "(⑅∫°ਊ°)∫";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(217, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(́◉◞౪◟◉)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "(´^ิ益^ิ｀ )";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(77, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "よけとるVC＃";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "GAMESTART";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -65,6 +111,10 @@
 
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- fukusyu0614/Form1.cs
+++ fukusyu0614/Form1.cs
@@ -12,11 +12,66 @@ namespace fukusyu0614
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        int getCount = 3;
+
+        //定数
+        const int itemCount = 3;
+
+        enum SCENE
+        {
+            TITLE,
+            GAMEPLAY,
+            GAMEOVER,
+            CLEAR,
+            NONE
+        }
+        /// <summary>
+        /// 現在のシーン
+        /// </summary>
+        SCENE nowScene;
+
+        /// <summary>
+        /// 切り替えたいシーン
+        /// </summary>
+        SCENE nextScene;
+
+
+        int [] vx =new int[itemCount];
+        int[] vy = new int[itemCount];
+        //配列100個分のラベル
+        Label[] labels = new Label[itemCount];
+
+        private static Random rand = new Random();
+
+
+
         public Form1()
         {
+            //基本的に↓コレの下に書く
             InitializeComponent();
+
+            nextScene = SCENE.TITLE;
+            nowScene = SCENE.NONE;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                //メモリとして使うためこっちも大事
+                labels[i] = new Label();
+                labels[i].AutoSize = true;
+                labels[i].Text = "○";
+                Controls.Add(labels[i]);
+                labels[i].Font = label1.Font;
+                labels[i].ForeColor = label1.ForeColor;
+                labels[i].Left = rand.Next(ClientSize.Width - label1.Width);
+                labels[i].Top = rand.Next(ClientSize.Height - label1.Height);
+                vx[i] = rand.Next(-5, 6);
+                vy[i] = rand.Next(-5, 6);
+            }
+
+
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Height - label1.Height);
+           
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -24,27 +79,95 @@ namespace fukusyu0614
 
         }
 
+        void initProc()
+        {
+
+            //nextSceneがNONEだったら、何もしない
+            if (nextScene == SCENE.NONE) return;
+
+            nowScene = nextScene;
+            nextScene = SCENE.NONE;
+            switch(nowScene)
+            {
+                case SCENE.TITLE:
+                    label4.Visible = true;
+                    button1.Visible=true; 
+                    break;
+                case SCENE.GAMEPLAY:
+                    label4.Visible = false;
+                    button1.Visible = false;
+                    getCount = itemCount;
+                    break;
+            }
+
+        }
+
+        void updateProc()
+        {
+            if (nowScene == SCENE.GAMEPLAY)
+            {
+                updateGame();
+            }
+        }
+
+        void updateGame()
+        {
+            for (int i = 0; i < itemCount; i++)
+            {
+                labels[i].Left += vx[i];
+                labels[i].Top += vy[i];
+
+                if (labels[i].Left < 0)
+                {
+                    vx[i] = Math.Abs(vx[i]);
+                }
+                if (labels[i].Top < 0)
+                {
+                    vy[i] = Math.Abs(vy[i]);
+                }
+                if (labels[i].Right > ClientSize.Width - labels[i].Width)
+                {
+                    vx[i] = -Math.Abs(vx[i]);
+                }
+                if (labels[i].Bottom > ClientSize.Height - labels[i].Height)
+                {
+                    vy[i] = -Math.Abs(vy[i]);
+                }
+
+                Point mp = PointToClient(MousePosition);
+                if ((mp.X >= labels[i].Left)
+                    && (mp.X < labels[i].Right)
+                    && (mp.Y >= labels[i].Top)
+                    && (mp.Y < labels[i].Bottom)
+                    )
+                {
+                    labels[i].Visible = false;
+                    getCount--;
+                    if (getCount<=0)
+                    {
+                        nextScene = SCENE.CLEAR;
+                    }
+                }
+            }
+        }
+
+       /* void CLEAR
+        {
+
+        }
+        */
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Left += vx;
-            label1.Top += vy;
+            initProc();
+            updateProc();
 
-            if(label1.Left<0)
-            {
-                vx = -vx;
-            }
-            if(label1.Top<0)
-            {
-                vy = -vy;
-            }
-            if(label1.Left>ClientSize.Width-label1.Width)
-            {
-                vx = -Math.Abs(vx);
-            }
-            if(label1.Top>ClientSize.Height-label1.Height)
-            {
-                vy = -Math.Abs(vy);
-            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nextScene = SCENE.GAMEPLAY;
         }
     }
 }
